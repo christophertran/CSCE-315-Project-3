dab829f4137766bcacf78592df7c7b419592c2bd
GradualSyrup <68757075+GradualSyrup@users.noreply.github.com>
2021-04-10 17:49:19 -0500

    addItem now takes in data instead of using magic

--- frontend/js/search.js
+++ frontend/js/search.js
@@ -1,6 +1,13 @@
 function search_test() {
     clearList();
-    addItem();
+    addItem({
+        title: 'Biden’s new Supreme Court commission is a win for the Federalist Society - Vox.com',
+        author: 'Ian Millhiser',
+        description: 'The president’s new commission won’t fix anything.',
+        url: 'https://www.vox.com/2021/4/10/22375792/supreme-court-biden-commission-reform-court-packing-federalist-society',
+        image: 'https://cdn.vox-cdn.com/thumbor/sgRBwypAhXuYeMjHIJ9IbZk5KBY=/0x258:3603x2144/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/22294571/1230698067.jpg',
+        published: '2021-04-10T12:30:00Z'
+    }); // Switch to addFeed() when backend is connected
 }
 function clearList() {
     var ul = document.getElementById("dynamic-list");
@@ -10,17 +17,7 @@ function clearList() {
         ul.removeChild(items[i]); // They do the same thing
     }
 }
-function addItem() {
-
-    var articleInfo = {
-        title: 'Biden’s new Supreme Court commission is a win for the Federalist Society - Vox.com',
-        author: 'Ian Millhiser',
-        description: 'The president’s new commission won’t fix anything.',
-        url: 'https://www.vox.com/2021/4/10/22375792/supreme-court-biden-commission-reform-court-packing-federalist-society',
-        image: 'https://cdn.vox-cdn.com/thumbor/sgRBwypAhXuYeMjHIJ9IbZk5KBY=/0x258:3603x2144/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/22294571/1230698067.jpg',
-        published: '2021-04-10T12:30:00Z'
-    };
-
+function addItem(articleInfo) {
     // get information form the html about the current list and the what the user inputted in the text box
 	var ul = document.getElementById("dynamic-list");
 	var candidate = document.getElementById("search-box");
@@ -64,7 +61,6 @@ function addItem() {
     linker.setAttribute('class', "article-text");
     linker.setAttribute('target', "_blank");
     li.setAttribute('id', candidate.value); // set id of this list item to be the searched value
-	//li.appendChild(document.createTextNode(candidate.value));
     
     cardBodyDiv.appendChild(articleTitle);
     cardBodyDiv.appendChild(blurb);
@@ -78,24 +74,8 @@ function addItem() {
     li.appendChild(linker);
 	ul.appendChild(li);
 }
-/*
-            <li>
-				<div style="padding-top: 10px; max-width: 850px;" class="card mb-3">
-					<div class="row g-0">
-						<div class="col-md-4">
-							<img src="painting.jpg" alt="...">
-						</div>
-						<div class="col-md-8">
-							<div class="card-body">
-						  		<h5 class="card-title">John Doe, Rep-TX</h5>
-						  		<p class="card-text"> Antiochus XII Dionysus (died 82 BC) was a Seleucid ruler who reigned as King of Syria during the Hellenistic period from 87 BC until his death. He was the youngest
-								  son of Antiochus VIII, who was assassinated in 96 BC. Antiochus XII's four brothers laid claim to the throne. His brother Philip I defeated his brother Demetrius III in 87 BC and remained
-								  in the Syrian capital Antioch.within inner Syria. He focused his attention on Syria's southern reaches into which the Judaeans and Nabataeans sought to expand, reinforced his southern frontier, and fought two campaigns against Nabataea and Judea.
-								</p>
-						 		<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-							</div>
-					  	</div>
-					</div>
-				</div>
-			</li>
-*/
+function addFeed(feedArray) {
+    for (article in feedArray) {
+        addItem(article);
+    }
+}
