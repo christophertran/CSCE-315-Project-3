04dd92dba9ee1758e24f6e089a9cfdef83aa900f
Christopher Tran <ctran457@gmail.com>
2021-04-12 10:43:45 -0500

    Able to search for politicians and display their articles and tweets on a page

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(express.json())
+
 const indexRoutes = require('./routes/index');
-const autocompleteRoutes = require('./routes/autocomplete');
-const searchRoutes = require('./routes/search');
-const twitterRoutes = require('./routes/twitterfeed');
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.use(indexRoutes);
-app.use("/autocomplete", autocompleteRoutes);
-app.use("/search", searchRoutes);
-app.use("/twitter-feed", twitterRoutes);
 
 app.listen(process.env.PORT || 3000, process.env.IP, () => {
     console.log('PoliLime has been served.');
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
+const backend = require('../backend/backend');
+const bk = new backend();
+
 router.get('/', (req, res) => {
     res.render('landing');
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
+})
+
+router.get('/autocomplete', (req, res) => {
+    bk.getNamesFromDatabase().then((names) => {
+        res.json(names);
+    }).catch((error) => {
+        console.error(error);
+    });
+    bk.disconnect();
+});
+
+router.get('/search', (req, res) => {
+    bk.getUserTwitterScreenNameByName(req.query.politicianName.toLowerCase()).then((screenName) => {
+        bk.getArticlesByName(req.query.politicianName.toLowerCase(), 20).then((articles) => {
+            res.render("politician", { articles: articles, screenName: screenName });
+        }).catch((error) => {
+            console.error(error);
+        });
+    }).catch((error) => {
+        console.error(error);
+    });
 });
 
 module.exports = router;
