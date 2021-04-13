9546de8926c752f8b0adaaca68d45142e3cc4700
Christopher Tran <ctran457@gmail.com>
2021-04-13 11:16:43 -0500

    Added ability to click map and get politicians from that state to display from backend

--- public/scripts/usmap/usmap.js
+++ public/scripts/usmap/usmap.js
@@ -1,17 +1,40 @@
 $(document).ready(function () {
     $('#map').usmap({
-        'mouseoverState': {
-            'HI': function (event, data) {
-                //return false;
-            }
-        },
+        // 'mouseoverState': {
+        //     'HI': function (event, data) {
+        //         //return false;
+        //     }
+        // },
+
+        // 'mouseover': function (event, data) {
+        // },
 
         'click': function (event, data) {
-            $('#alert')
-                .text('Click ' + data.name)
+            axios.get("/state/" + data.name).then((result) => {
+                var out = '';
+                result.data.forEach(element => {
+                    out += ' | ' + element.name.toUpperCase();
+                });
+                out += ' | ';
+
+                $('#alert')
+                .text(out)
                 .stop()
                 .css('backgroundColor', 'yellow')
                 .animate({ backgroundColor: '#ddd' }, 1000);
-        }
+            }).catch((error) => {
+                console.log(error);
+            });
+        },
+
+        'stateHoverStyles': {
+            fill: '#198754'
+        },
+
+        'labelBackingHoverStyles': {
+            fill: '#198754'
+        },
+
+        showLabels: true
     });
 });
--- routes/index.js
+++ routes/index.js
@@ -33,4 +33,72 @@ router.get('/search', (req, res) => {
     });
 });
 
+router.get('/state/:stateAbbrev', (req, res) => {
+    const states = {
+        "AL": "Alabama",
+        "AK": "Alaska",
+        "AS": "American Samoa",
+        "AZ": "Arizona",
+        "AR": "Arkansas",
+        "CA": "California",
+        "CO": "Colorado",
+        "CT": "Connecticut",
+        "DE": "Delaware",
+        "DC": "District Of Columbia",
+        "FM": "Federated States Of Micronesia",
+        "FL": "Florida",
+        "GA": "Georgia",
+        "GU": "Guam",
+        "HI": "Hawaii",
+        "ID": "Idaho",
+        "IL": "Illinois",
+        "IN": "Indiana",
+        "IA": "Iowa",
+        "KS": "Kansas",
+        "KY": "Kentucky",
+        "LA": "Louisiana",
+        "ME": "Maine",
+        "MH": "Marshall Islands",
+        "MD": "Maryland",
+        "MA": "Massachusetts",
+        "MI": "Michigan",
+        "MN": "Minnesota",
+        "MS": "Mississippi",
+        "MO": "Missouri",
+        "MT": "Montana",
+        "NE": "Nebraska",
+        "NV": "Nevada",
+        "NH": "New Hampshire",
+        "NJ": "New Jersey",
+        "NM": "New Mexico",
+        "NY": "New York",
+        "NC": "North Carolina",
+        "ND": "North Dakota",
+        "MP": "Northern Mariana Islands",
+        "OH": "Ohio",
+        "OK": "Oklahoma",
+        "OR": "Oregon",
+        "PW": "Palau",
+        "PA": "Pennsylvania",
+        "PR": "Puerto Rico",
+        "RI": "Rhode Island",
+        "SC": "South Carolina",
+        "SD": "South Dakota",
+        "TN": "Tennessee",
+        "TX": "Texas",
+        "UT": "Utah",
+        "VT": "Vermont",
+        "VI": "Virgin Islands",
+        "VA": "Virginia",
+        "WA": "Washington",
+        "WV": "West Virginia",
+        "WI": "Wisconsin",
+        "WY": "Wyoming"
+    };
+
+    bk.getNamesFromDatabaseByState(states[req.params.stateAbbrev]).then((politicians) => {
+        res.json(politicians);
+    });
+});
+
 module.exports = router;
