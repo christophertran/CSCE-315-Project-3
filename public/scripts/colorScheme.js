1c4ea470b19a55fd4e489cf3ad3fa10ce5ce2b9a
Hoa Nguyen <hoaannguyen07@tamu.edu>
2021-04-16 19:45:08 -0500

    put in toggle color scheme between dark and light but still a few small bugs to figure out.

---
+++ public/scripts/colorScheme.js
@@ -0,0 +1,77 @@
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+
+// function to toggle between light and dark theme
+function toggleTheme() {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-light');
+        localStorage.setItem('theme', 'theme-light');
+
+        // set color scheme
+        var pageColorScheme = document.getElementById("colorScheme");
+        console.log(pageColorScheme.getAttribute('href'));
+        pageColorScheme.setAttribute('href', "/stylesheets/light.css");
+
+        // set twitter theme
+        var twitter = document.getElementById('twitter-timeline');
+        twitter.setAttribute('data-theme', "light");
+
+        console.log(pageColorScheme.getAttribute('href'));
+
+        document.getElementById('slider').checked = false;
+    } else {
+        setTheme('theme-dark');
+        localStorage.setItem('theme', 'theme-dark');
+        
+        // set color scheme
+        var pageColorScheme = document.getElementById("colorScheme");
+        console.log(pageColorScheme.getAttribute('href'));
+        pageColorScheme.setAttribute('href', "/stylesheets/dark.css");
+
+        // set twitter theme
+        var twitter = document.getElementById('twitter-timeline');
+        twitter.setAttribute('data-theme', "dark");
+
+        console.log(pageColorScheme.getAttribute('href'));
+
+        document.getElementById('slider').checked = true;
+    }
+}
+
+// Immediately invoked function to set the theme on initial load
+(function () {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-dark');
+        localStorage.setItem('theme', 'theme-dark');
+        
+        // set color scheme
+        var pageColorScheme = document.getElementById("colorScheme");
+        console.log(pageColorScheme.getAttribute('href'));
+        pageColorScheme.setAttribute('href', "/stylesheets/dark.css");
+        console.log(pageColorScheme.getAttribute('href'));
+
+        // set twitter theme
+        var twitter = document.getElementById('twitter-timeline');
+        twitter.setAttribute('data-theme', "dark");
+
+        document.getElementById('slider').checked = true;
+    } else {
+        setTheme('theme-light');
+        localStorage.setItem('theme', 'theme-light');
+
+        // set color scheme
+        var pageColorScheme = document.getElementById("colorScheme");
+        console.log(pageColorScheme.getAttribute('href'));
+        pageColorScheme.setAttribute('href', "/stylesheets/light.css");
+        console.log(pageColorScheme.getAttribute('href'));
+
+        // set twitter theme
+        var twitter = document.getElementById('twitter-timeline');
+        twitter.setAttribute('data-theme', "light");
+
+        document.getElementById('slider').checked = false;
+    }
+})();
