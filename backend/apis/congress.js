ac1adcb59d3c5803f92ce74069292ab8b378d100
Christopher Tran <ctran457@gmail.com>
2021-04-20 23:08:56 -0500

    Added some caching to prevent calling congress API every single time

--- backend/apis/congress.js
+++ backend/apis/congress.js
@@ -4,22 +4,62 @@ module.exports = class currents {
     constructor() {
         this.congressapi = new CongressAPI(process.env.CONGRESS_API_KEY);
         this.congressNumber = 117;
+
+        this.senate = null;
+        this.senateTimeout = null;
+        this.senateTimeoutTime = 15 * 60 * 1000; // n minutes * 60 seconds * 1000 milliseconds // 5 minute timeout
+
+        this.house = null;
+        this.houseTimeout = null;
+        this.houseTimeoutTime = 15 * 60 * 1000; // n minutes * 60 seconds * 1000 milliseconds // 5 minute timeout
     }
 
     async getSenateMembers() {
+        if (this.senate == null || this.senateTimeout == null) {
+            this.senateTimeout = setTimeout(() => {
+                this.senateTimeout = null;
+            }, this.senateTimeoutTime);
+
+            return this.getSenateMembersFromAPI().then((senate) => {
+                this.senate = senate;
+                return this.senate;
+            });
+        } else {
+            return this.senate;
+        }
+    }
+
+    async getSenateMembersFromAPI() {
         return this.congressapi.memberLists({
             congressNumber: this.congressNumber,
             chamber: 'senate'
-        }).then(function (res) {
+        }).then((res) => {
+            console.log('Refreshing senate member information from ProPublica Congress API');
             return res.results[0].members;
         });
     }
 
     async getHouseMembers() {
+        if (this.house == null || this.houseTimeout == null) {
+            this.houseTimeout = setTimeout(() => {
+                this.houseTimeout = null;
+            }, this.houseTimeoutTime);
+
+            return this.getHouseMembersFromAPI().then((house) => {
+                this.house = house;
+                return this.house;
+            });
+        } else {
+            return this.house;
+        }
+    }
+
+    async getHouseMembersFromAPI() {
         return this.congressapi.memberLists({
             congressNumber: this.congressNumber,
             chamber: 'house'
-        }).then(function (res) {
+        }).then((res) => {
+            console.log('Refreshing house member information from ProPublica Congress API');
             return res.results[0].members;
         });
     }
--- routes/index.js
+++ routes/index.js
@@ -10,9 +10,81 @@ router.get('/', (req, res) => {
     res.render('landing');
 });
 
+const states = {
+    "AL": "Alabama",
+    "AK": "Alaska",
+    "AS": "American Samoa",
+    "AZ": "Arizona",
+    "AR": "Arkansas",
+    "CA": "California",
+    "CO": "Colorado",
+    "CT": "Connecticut",
+    "DE": "Delaware",
+    "DC": "District Of Columbia",
+    "FM": "Federated States Of Micronesia",
+    "FL": "Florida",
+    "GA": "Georgia",
+    "GU": "Guam",
+    "HI": "Hawaii",
+    "ID": "Idaho",
+    "IL": "Illinois",
+    "IN": "Indiana",
+    "IA": "Iowa",
+    "KS": "Kansas",
+    "KY": "Kentucky",
+    "LA": "Louisiana",
+    "ME": "Maine",
+    "MH": "Marshall Islands",
+    "MD": "Maryland",
+    "MA": "Massachusetts",
+    "MI": "Michigan",
+    "MN": "Minnesota",
+    "MS": "Mississippi",
+    "MO": "Missouri",
+    "MT": "Montana",
+    "NE": "Nebraska",
+    "NV": "Nevada",
+    "NH": "New Hampshire",
+    "NJ": "New Jersey",
+    "NM": "New Mexico",
+    "NY": "New York",
+    "NC": "North Carolina",
+    "ND": "North Dakota",
+    "MP": "Northern Mariana Islands",
+    "OH": "Ohio",
+    "OK": "Oklahoma",
+    "OR": "Oregon",
+    "PW": "Palau",
+    "PA": "Pennsylvania",
+    "PR": "Puerto Rico",
+    "RI": "Rhode Island",
+    "SC": "South Carolina",
+    "SD": "South Dakota",
+    "TN": "Tennessee",
+    "TX": "Texas",
+    "UT": "Utah",
+    "VT": "Vermont",
+    "VI": "Virgin Islands",
+    "VA": "Virginia",
+    "WA": "Washington",
+    "WV": "West Virginia",
+    "WI": "Wisconsin",
+    "WY": "Wyoming"
+};
+
+const affiliations = {
+    "R": "Republican",
+    "D": "Democrat"
+}
+
+const genders = {
+    "M": "Male", 
+    "F": "Female"
+}
+
 router.get('/politicians', (req, res) => {
     bk.getCongressMembersNamesOrganizedByState().then((members) => {
-        res.render('politicians', { members: members });
+        res.render('politicians', { members: members, states: states });
     });
 });
 
@@ -45,7 +117,7 @@ router.get('/search', (req, res) => {
         const info = members[req.query.politicianName.toLowerCase()];
         if (info) {
             bk.getArticlesByName(req.query.politicianName.toLowerCase(), 20).then((articles) => {
-                res.render("politician", { info: info, articles: articles, searchInput: req.query.politicianName });
+                res.render("politician", { info: info, articles: articles, searchInput: req.query.politicianName, states: states, affiliations: affiliations, genders: genders });
             }).catch((error) => {
                 console.error(error);
             });
